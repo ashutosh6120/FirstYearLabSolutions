bbd2ba465d986bb8560f8bccbd21b2121d903e02
Ashutosh Dodamani <59788077+ashutosh6120@users.noreply.github.com>
2021-03-29 18:59:40 +0530

    program 6 added

---
+++ labCycle-2program6.c
@@ -0,0 +1,13 @@
+// write a program to print the size of various data types in C
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\nsize of int = %d\n",sizeof(int));
+    printf("\nsize of float = %d\n",sizeof(float));
+    printf("\nsize of double = %d\n",sizeof(double));
+    printf("\nsize of char = %d\n",sizeof(char));
+    printf("\nsize of longint = %d\n",sizeof(long int));
+    return 0;
+}
